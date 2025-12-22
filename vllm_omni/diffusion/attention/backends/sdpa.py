77c9afbbf2b7c4d53cbc106eca2a680e71e5bfb3
KexiongYu <yukexiong1@huawei.com>
2025-12-22 17:25:23 +0800

    add fused operator attn_forward on NPU
    Signed-off-by: KexiongYu <yukexiong1@huawei.com>

--- vllm_omni/diffusion/attention/backends/sdpa.py
+++ vllm_omni/diffusion/attention/backends/sdpa.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+from collections.abc import Callable
+from typing import Any
+
 import torch
 from vllm.logger import init_logger
 
@@ -9,6 +12,7 @@ from vllm_omni.diffusion.attention.backends.abstract import (
     AttentionImpl,
     AttentionMetadata,
 )
+from vllm_omni.utils.platform_utils import detect_device_type, is_rocm
 
 logger = init_logger(__name__)
 
@@ -42,8 +46,33 @@ class SDPAImpl(AttentionImpl):
     ) -> None:
         self.causal = causal
         self.softmax_scale = softmax_scale
+        self.is_cuda = detect_device_type() == "cuda"
+        self.is_npu = detect_device_type == "npu"
+        self._forward_method = self.dispatch_forward()
 
-    def forward(
+    def dispatch_forward(self) -> Callable:
+        if is_rocm():
+            return self.forward_hip
+        elif self.is_cuda:
+            return self.forward_cuda
+        elif self.is_npu:
+            return self.forward_npu
+        else:
+            return self.forward_native
+
+    def forward(self, *args, **kwargs) -> Any:
+        return self._forward_method(*args, **kwargs)
+
+    def forward_hip(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        attn_metadata: AttentionMetadata = None,
+    ) -> torch.Tensor:
+        return self.forward_cuda(query, key, value, attn_metadata)
+
+    def forward_cuda(
         self,
         query: torch.Tensor,
         key: torch.Tensor,
@@ -64,3 +93,30 @@ class SDPAImpl(AttentionImpl):
         )
         out = output.permute(0, 2, 1, 3)
         return out
+
+    def forward_npu(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        attn_metadata: AttentionMetadata = None,
+    ) -> torch.Tensor:
+        from mindiesd import attention_forward
+        query, key, value = (x.permute(0, 2, 1, 3) for x in (query, key, value))
+        attention_mask = attn_metadata.attn_mask if attn_metadata else None
+
+        output = attention_forward(
+            query, key, value, attn_mask=attention_mask, is_causal=False
+        )
+
+        out = output.permute(0, 2, 1, 3)
+        return out
+
+    def forward_native(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        attn_metadata: AttentionMetadata = None,
+    ) -> torch.Tensor:
+        return self.forward_cuda(query, key, value, attn_metadata)
